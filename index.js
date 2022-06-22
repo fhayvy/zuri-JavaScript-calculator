2e2edbdccd549d65d67b8778bf1ad1ae4efa14f7
mac <ezefavourchiamaka01@gmail.com>
2022-06-22 16:40:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+function add(numA, numB){
+    return numA + numB;
+}
+function subtract(numA, numB){
+    return numA - numB;
+}
+function multiply(numA, numB){
+    return numA * numB;
+}
+function divide(numA, numB){
+    return numA / numB;
+}
+function modulus(numA, numB){
+    return numA % numB;
+}
+
+
+function userComputation(){
+    var numA = parseFloat(prompt("Please input your first number:"))
+    var operator = prompt("Please choose an operator (+, -, *, /, %):")
+    var numB = parseFloat(prompt("Please input another number:"))
+    var result = 0;
+    if (operator == "+"){
+        result = add(numA, numB);
+    }
+    else if (operator == "-"){
+        result = subtract(numA, numB);
+    }
+    else if (operator == "*"){
+        result = multiply(numA, numB);
+    }
+    else if (operator == "/"){
+        result = divide(numA, numB);
+    }
+    else if (operator == "%"){
+        result = modulus(numA, numB);
+    }
+    else{
+        alert("The operator you entered was invalid! Please pick an operator from the options:")
+    }
+    var confirm = window.confirm(`The value of ${numA} ${operator} ${numB} is ${result}, Do you want to perform another calculation?`);
+
+    if (confirm) {
+        userComputation()
+    } else {
+        alert("Thank you for trying our calculator out.")
+    }
+}
+
+userComputation()
